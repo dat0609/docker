430079573390f95ce6401bca17dbd0943ea18c09
dat <you@example.com>
2021-07-24 14:12:33 +0700

    docker

---
+++ src/main/java/com/lms/MainController.java
@@ -0,0 +1,13 @@
+package com.lms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+	@GetMapping("/")
+	public String getView() {
+		return "Hello";
+	}
+}
